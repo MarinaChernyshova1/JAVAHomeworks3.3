1fbf03464a85766ed5ce097cf6041495c1bfe7e6
MarinaCh <marina3123273@mail.ru>
2023-04-10 17:31:44 +0300

    Second commit

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,6 +1,8 @@
 public class CreditPaymentService {
-    public int calculate(int summaKredita, double procent, int month) {
-        double koeff = procent + (procent / (Math.pow((1 + procent), month) - 1));
+    public int calculate(int summaKredita, double procent, int year) {
+        int month = year * 12;
+        double monthlyRate = procent / 12 / 100;
+        double koeff = monthlyRate * ((Math.pow((1 + monthlyRate), month)) / ((Math.pow(1 + monthlyRate, month)) - 1));
         int result = (int) (summaKredita * koeff);
         return result;
     }
--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,20 @@ public class Main {
         System.out.println("Считаем ежемесячный платёж " +
                 " если сумма кредита " + 1_000_000 +
                 " процентная ставка " + 9.99 + "%" +
-                " срок кредитования " + 12 + " месяцев");
-        System.out.println(service.calculate(1000000, 9.99, 12));
+                " срок кредитования " + 1 + " год(а)");
+        System.out.println(service.calculate(1000000, 9.99, 1));
+        System.out.println();
+        System.out.println("Считаем ежемесячный платёж " +
+                " если сумма кредита " + 1_000_000 +
+                " процентная ставка " + 9.99 + "%" +
+                " срок кредитования " + 2 + " год(а)");
+        System.out.println(service.calculate(1000000, 9.99, 2));
+        System.out.println();
+        System.out.println("Считаем ежемесячный платёж " +
+                " если сумма кредита " + 1_000_000 +
+                " процентная ставка " + 9.99 + "%" +
+                " срок кредитования " + 3 + " год(а)");
+        System.out.println(service.calculate(1000000, 9.99, 3));
 
     }
 }
